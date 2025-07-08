261cabdda73da93e81e01f7d324cdb1305e211a4
MHT <hassantariqirshad@gmail.com>
2025-07-08 14:48:03 +0500

    test AST PRINTER

--- lox/AstPrinter.java
+++ lox/AstPrinter.java
@@ -36,7 +36,7 @@ class AstPrinter implements Expr.Visitor<String> {
     }
     
 
-    
+
     private String parenthesize(String name, Expr...exprs){
         
         StringBuilder builder = new StringBuilder();
@@ -51,6 +51,19 @@ class AstPrinter implements Expr.Visitor<String> {
         builder.append(")");
 
         return builder.toString();
+
+    }
+
+    public static void main(String[] args){
+
+        Expr expression = new Expr.Binary(
+            new Expr.Unary(
+                new Token(TokenType.MINUS, "-", null, 1),
+                new Expr.Literal(123)), 
+             new Token(TokenType.STAR, "*",null, 1),
+              new Expr.Grouping(new Expr.Literal(45.67)));
+
+        System.out.println(new AstPrinter().print(expression));
 
     }
     
