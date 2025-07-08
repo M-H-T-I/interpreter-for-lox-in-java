2d7a0dcdd5519ddc67b5415ca6a4b6e215d743d7
MHT <hassantariqirshad@gmail.com>
2025-07-08 12:52:11 +0500

    create the helper function parenthesize

--- lox/AstPrinter.java
+++ lox/AstPrinter.java
@@ -36,6 +36,22 @@ class AstPrinter implements Expr.Visitor<String> {
     }
     
 
+    
+    private String parenthesize(String name, Expr...exprs){
+        
+        StringBuilder builder = new StringBuilder();
+        
+        builder.append("(").append(name);
+        
+        for (Expr expr: exprs){
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
 
+        builder.append(")");
+
+        return builder.toString();
+
+    }
     
 }
