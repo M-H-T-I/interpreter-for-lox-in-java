36e35c76e09f293019eb1f679c7ae7cda01b79bf
MHT <hassantariqirshad@gmail.com>
2025-07-09 06:48:00 +0500

    create rule for equality

--- lox/Parser.java
+++ lox/Parser.java
@@ -12,7 +12,29 @@ class Parser {
         this.tokens = tokens;
     } 
 
+    // rule for expression coverted to code
     private Expr expression(){
         return equality();
     }
+    
+    // rule for equality coverted to code
+    private Expr equality(){
+
+        // maps to the first comparison non-terminal in the grammar rule for equality
+        Expr expr = comparison();
+
+        // runs the remaining part of the rule as many time as == or != occur
+        while(match(BANG_EQUAL, EQUAL_EQUAL)){
+
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right); // converts the comparison statement into a binary token
+        
+
+        }
+
+        return expr;
+    }
+
+
 }
