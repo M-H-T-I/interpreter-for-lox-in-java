2024e4a99bf869110a6140056532bada43a6d685
MHT <hassantariqirshad@gmail.com>
2025-07-10 11:41:49 +0500

    handle literals in the interpreter

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -2,6 +2,12 @@ package lox;
 
 class Interpreter implements Expr.Visitor<Object> {
 
-    
+    // handling the conversion of literals from syntax tree form to runtime values
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr){
+
+        return expr.value;
+        
+    }
     
 }
