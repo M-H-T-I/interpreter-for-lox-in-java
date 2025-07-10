9db052a396faa0971a1b061be85fd6abc66fe398
MHT <hassantariqirshad@gmail.com>
2025-07-10 12:09:20 +0500

    Handle conversion unary expressions

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -2,7 +2,7 @@ package lox;
 
 class Interpreter implements Expr.Visitor<Object> {
 
-    // handling the conversion of literals from syntax tree form to runtime values
+    // Literal conversion to runtime value:
     @Override
     public Object visitLiteralExpr(Expr.Literal expr){
 
@@ -10,7 +10,7 @@ class Interpreter implements Expr.Visitor<Object> {
 
     }
 
-    // handling conversion of groupings
+    // Grouping conversion to runtime value:
     @Override 
     public Object visitGroupingExpr(Expr.Grouping expr){
 
@@ -18,12 +18,48 @@ class Interpreter implements Expr.Visitor<Object> {
 
     }
 
+    // Unary Expression conversion:
+    public Object visitUnaryExpr(Expr.Unary expr){
 
-    //helper method evaluate
-    private Object evaluate(Expr expr){
+        Object right = evaluate(expr.right); // calls the appropriate accept method letting the expression call its own vist method
+
+        switch (expr.operator.type) {
+
+            case BANG:
+
+                return !isTruthy(right);
+
+            case MINUS:
+                return -(double)right;
         
+        }
+
+        //unreacable
+        return null;
+    } 
+
+
+    // Helper Methods
+
+    //evaluate: calls the expression's accept method allowing the expression to call the appropriate type of method
+    private Object evaluate(Expr expr){
+
         return expr.accept(this);
+
     }
+
+    //isTruthy: determines what value is truth and coversely what is falsey
+    private boolean isTruthy(Object object){
+
+        // only null (nil) and false are falsey
+
+        if(object == null) return false;
+        if (object instanceof Boolean) return (boolean) object;
+
+        return true;
+
+    }
+
 
 
 }
