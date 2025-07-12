08878d267733945dd0cd0ceece144093246f1faa
MHT <hassantariqirshad@gmail.com>
2025-07-12 16:51:07 +0500

    integrate interpreter with existing program

--- lox/Lox.java
+++ lox/Lox.java
@@ -11,7 +11,9 @@ import java.util.List;
 
 public class Lox {
 
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException{
         if (args.length > 1){
@@ -33,6 +35,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
 
     }
 
@@ -66,7 +69,7 @@ public class Lox {
         // stop if there is a syntax error
         if (hadError)return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpreter(expression);
 
     }
 
@@ -93,6 +96,11 @@ public class Lox {
         System.err.println("[line " + line + "] Error" + where + ": " + message );
         hadError = true;
 
+    }
+
+    static void runtimeError(RuntimeError error){
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
 
