ed766826576b7415ebe5929252cf462be6c59eb1
MHT <hassantariqirshad@gmail.com>
2025-07-14 11:07:08 +0500

    meta code for stmt base class

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -25,6 +25,11 @@ public class GenerateAst {
             "Unary: Token operator, Expr right"
         ));
 
+        defineAst(outputDir, "Stmt", Arrays.asList(
+            "Expression : Expr expression",
+                "Print : Expr expression"
+        ));
+
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException{
