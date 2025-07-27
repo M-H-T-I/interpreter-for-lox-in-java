3e8b499df71882cdc6888bac2f6f8c5f7e4a48c0
MHT <hassantariqirshad@gmail.com>
2025-07-27 17:55:43 +0500

    add native clock function

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -5,8 +5,30 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
+    final Environment globals = new Environment();
+
     // used to store variables and their values
-    private Environment environment = new Environment();
+    private Environment environment = globals;
+
+
+    Interpreter(){
+        globals.define("clock", new LoxCallable() {
+            
+            @Override
+            public int arity(){return 0;}
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments){
+                return (double) System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString(){return "<native fn>";}
+
+        });
+    }
+
+
 
     // Statements ----------
 
