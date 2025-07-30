7c93e3a92f03dc5359593435d98e405a2657f490
MHT <hassantariqirshad@gmail.com>
2025-07-30 13:02:52 +0500

    add variable expr and variable stmt visitiors along with the required helper methods

--- lox/Resolver.java
+++ lox/Resolver.java
@@ -9,6 +9,7 @@ import java.util.Stack;
 class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     
     private final Interpreter interpreter;
+    private final Stack<Map<String,Boolean>> scopes = new Stack<>();
 
     Resolver(Interpreter interpreter){
 
@@ -26,6 +27,32 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     }
 
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt){
+
+        //declare the variable
+        declare(stmt.name);
+        if(stmt.initializer != null){
+            // if present resolve the initializer
+            resolve(stmt.initializer);
+        }
+
+        define(stmt.name);
+        return null;
+
+    }
+
+    @Override 
+    public Void visitVariableExpr(Expr.Variable expr){
+        if(!scopes.isEmpty() && scopes.peek().get(expr.name.lexeme) == Boolean.FALSE){
+            Lox.error(expr.name, "Can't read local variable in its own initializer");   
+        }
+
+        resolveLocal(expr, expr.name);
+        return null;
+    }
+
+
     //--------------------------- Helper methods -------------------------
 
     // walks a list of statements and resolves each one: calls resolve method below
@@ -37,6 +64,48 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     // calls the accept method of statements
     private void resolve(Stmt stmt){
+
         stmt.accept(this);
     } 
+
+    // sees which scope has the variable if it does exist  
+    private void resolveLocal(Expr expr, Token name){
+        for (int i = scopes.size() - 1; i >= 0; i--){
+            if (scopes.get(i).containsKey(name.lexeme)){
+                // we asssume the variable is global
+                interpreter.resolve(expr, scopes.size() - 1 - i);
+                return;
+            }
+        }
+    }
+
+    // creates a block scope
+    private void beginScope(){
+        scopes.push(new HashMap<String, Boolean>());
+    }
+
+    //ends (terminates) a scope
+    private void endScope(){
+
+        scopes.pop();
+    }
+
+
+    // declares a variable in scope 
+    private void declare(Token name){
+        // in global scope hence no need to resolves
+        if(scopes.isEmpty()) return;
+
+        // gets the reference to the innermost scope
+        Map<String, Boolean> scope = scopes.peek();
+        // declares the variable in said scope
+        scope.put(name.lexeme,false);
+    }
+
+    //defines a variable's value; marks its value to true
+    private void define(Token name){
+        if (scopes.isEmpty())return;
+        scopes.peek().put(name.lexeme, true);
+    }
+
 }
