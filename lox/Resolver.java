5b3b88f24b6072e2c1f00b244fbd2b19d0e586a7
MHT <hassantariqirshad@gmail.com>
2025-08-21 15:12:52 +0500

    implement set functionality and methods

--- lox/Resolver.java
+++ lox/Resolver.java
@@ -16,7 +16,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     private enum FunctionType{
         NONE,
-        FUNCTION
+        FUNCTION,
+        METHOD
     }
 
     Resolver(Interpreter interpreter){  
@@ -29,6 +30,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitClassStmt(Stmt.Class stmt){
         declare(stmt.name);
         define(stmt.name);
+
+        for (Stmt.Function method: stmt.methods){
+            FunctionType declaration = FunctionType.METHOD;
+            resolveFunction(method, declaration);
+        }
 
         return null;
     }
@@ -156,6 +162,14 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     @Override
     public Void visitGetExpr(Expr.Get expr){
         resolve(expr);
+        return null;
+    }
+
+    @Override
+    public Void visitSetExpr(Expr.Set expr){
+        resolve(expr.value);
+        resolve(expr.object);
+
         return null;
     }
 
